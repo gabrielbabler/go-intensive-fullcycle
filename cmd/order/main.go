dc813bbaded7f239fff0fb3a710b2d6942c5c1e8
Gabriel Babler <gjacobbabler@atlassian.com>
2023-07-19 23:18:26 -0300

    day 2 of go-intensive

---
+++ cmd/order/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/gabrielbabler/go-intensive-fullcycle/internal/infra/database"
+	"github.com/gabrielbabler/go-intensive-fullcycle/internal/usecase"
+	"github.com/gabrielbabler/go-intensive-fullcycle/pkg/rabbitmq"
+	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close() //await everything to be processed and then close
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, uc)
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc *usecase.CalculateFinalPrice) {
+	fmt.Println("Starting rabbitmq")
+	for msg := range msgChan {
+		var input usecase.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		output, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println("Mensagem processada e salva no banco:", output)
+	}
+}
